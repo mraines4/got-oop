0b7656782ccf92771fc3db17389a46b186cd48b0
Matt Raines <mraines4@gmail.com>
2019-02-20 11:21:03 -0500

    adds the avatar and tets it

---
+++ tests.py
@@ -0,0 +1,10 @@
+# shhh these arent "real" tests
+
+from character import Character
+
+# Character can be instantiated with name and avatar
+arya = Character("Arya Stark", "arya.png")
+jon = Character("Jon Snow", "jon.png")
+
+print(arya.name, arya.avatar)
+print(jon.name, jon.avatar)
